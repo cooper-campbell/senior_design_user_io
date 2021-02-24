a3ea85bc38a51ba94d29b308e695ce5dcf4f67ba
Cooper Campbell <campb332@purdue.edu>
2021-02-24 09:42:11 -0500

    Fix scaling

--- tests/interpolate.c
+++ tests/interpolate.c
@@ -15,19 +15,10 @@
 // Private vars
 uint16_t waveform[WAVEFORM_SIZE] = {0};
 uint16_t input_test[800] = {0};
-// anonymous structure to count waveform properly
-// almost out of ram already so time for bitfields
-struct {
-	uint32_t sample_counter : 4;
-	uint32_t removal_counter : 4;
-	uint32_t total_counter : 12;
-} waveform_counter;
+int sum = 0;
 
 // private declarations
 void reset_counters() {
-	waveform_counter.sample_counter = 0;
-	waveform_counter.removal_counter = 0;
-	waveform_counter.total_counter = 0;
 }
 
 int find_location(int x) {
@@ -35,53 +26,10 @@ int find_location(int x) {
 	return tmp + tmp/10 - tmp/100 -1;
 }
 
-void stream_touch_sample(uint16_t x, uint16_t y) {
-	// This is the math necessary to expand 800 samples into 1746.
-	// Need to manually add 2 points at the end to make it match the beginning.
-	int location = find_location(x);
-
-	// The y math scales the 480 pixels to be [0, 2^16-1] as per the specified interface.
-	waveform[location] = y; //136 * y + 17 * y/32;
-}
-
-void interpolate_waveform() {
-	reset_counters();
-	// So we double every sample, by adding 1 sample before it
-	// Next, every 10 samples we add a new one, skipping every 100 samples
-	uint16_t value_before = waveform[WAVEFORM_SIZE-1];
-	uint16_t value_after;
-	uint8_t inc = 1;
-	uint32_t sum = 0;
-
-	// It is easier to loop over every point given and interpolate between
-	int alocation = find_location(0);
-	int blocation = find_location(799);
-	int num = 2;
-	value_before = waveform[blocation];
-	value_after = waveform[alocation];
-	int index = 0;
-	for(int i = 0; i < 800; i++) {
-		for(int j = 1; j < num; j++) {
-			waveform[index++] = (j * (value_after - value_before))/num + value_before;
-		}
-		index++;
-		blocation = alocation;
-		alocation = find_location((i+1)%800);
-		num = alocation - blocation;
-		value_before = waveform[blocation];
-		value_after = waveform[alocation];
-	}
-	value_after = waveform[find_location(0)];
-	value_before = waveform[find_location(799)];
-	// add final two points to smooth towards the beginning.
-	waveform[WAVEFORM_SIZE-2] = (value_after - value_before) / 3 + value_before;
-	waveform[WAVEFORM_SIZE-1] = (2*(value_after - value_before)) / 3 + value_before;
-	waveform[0] = waveform[WAVEFORM_SIZE-1];
-	// Note that with this method, there are 3 interpolated points between the first input by the user and the last.
-
-	/*
+void normalize_waveform() {
 	// Now we start removing DC offset as best we can.
 	// also I know this has potential for weird maths bc of the unsigned/signed discrepancy.
+	printf("sum: %d\n", sum);
 	int16_t average_whole = sum/1746 - (2^15);
 	uint16_t average_remainder;
 	// avoid divide by zero
@@ -106,7 +54,6 @@ void interpolate_waveform() {
 	// I may have to get more precise by finding the remainder of the remainder division even.
 	multiplier = 1;
 	for(uint16_t i = 0; i < WAVEFORM_SIZE; i++) {
-
 		if(i % average_remainder == 0) {
 			if(waveform[i] >= 1) {
 				waveform[i] = waveform[i] - multiplier;
@@ -117,7 +64,52 @@ void interpolate_waveform() {
 			}
 		}
 	}
-	*/
+}
+
+void stream_touch_sample(uint16_t x, uint16_t y) {
+	// This is the math necessary to expand 800 samples into 1746.
+	// Need to manually add 2 points at the end to make it match the beginning.
+	int location = find_location(x);
+
+	// The y math scales the 480 pixels to be [0, 2^16-1] as per the specified interface.
+	waveform[location] = 136 * y + 17 * y/32;
+}
+
+void interpolate_waveform() {
+	reset_counters();
+	// So we double every sample, by adding 1 sample before it
+	// Next, every 10 samples we add a new one, skipping every 100 samples
+	uint16_t value_before = waveform[WAVEFORM_SIZE-1];
+	uint16_t value_after;
+	uint8_t inc = 1;
+	sum = 0;
+
+	// It is easier to loop over every point given and interpolate between
+	int alocation = find_location(0);
+	int blocation = find_location(799);
+	int num = 2;
+	value_before = waveform[blocation];
+	value_after = waveform[alocation];
+	int index = 0;
+	for(int i = 0; i < 800; i++) {
+		for(int j = 1; j < num; j++) {
+			waveform[index] = (j * (value_after - value_before))/num + value_before;
+			sum += waveform[index];
+			index++;
+		}
+		index++;
+		blocation = alocation;
+		alocation = find_location((i+1)%800);
+		num = alocation - blocation;
+		value_before = waveform[blocation];
+		value_after = waveform[alocation];
+	}
+	value_after = waveform[find_location(0)];
+	value_before = waveform[find_location(799)];
+	// add final two points to smooth towards the beginning.
+	waveform[WAVEFORM_SIZE-2] = (value_after - value_before) / 3 + value_before;
+	waveform[WAVEFORM_SIZE-1] = (2*(value_after - value_before)) / 3 + value_before;
+	waveform[0] = waveform[WAVEFORM_SIZE-1];
 }
 
 void print_waveform(FILE *fp, uint16_t *x, int size) {
@@ -128,7 +120,6 @@ void print_waveform(FILE *fp, uint16_t *x, int size) {
 }
 
 void run_test(FILE * fp) {
-	
 	for(int i = 0; i < 800; i++) {
 		stream_touch_sample(i, input_test[i]);
 	}
@@ -148,21 +139,39 @@ void main() {
 
 	// Test 1
 	for(int i = 0; i < 800; i++) { 
-		input_test[i] = i;
+		input_test[i] = i*480/800;
 	}
 
 	print_waveform(fp, input_test, 800);
 
 	run_test(fp);
 
+	normalize_waveform();
+	print_waveform(fp, waveform, WAVEFORM_SIZE);
+
+	// 2
 	for(int i = 0; i < 800; i++) {
-		input_test[i] = 400 + 399 * sin(2*M_PI * i / 800);
+		input_test[i] = 240 + 239 * sin(2*M_PI * i / 800);
 	}
 
 	print_waveform(fp, input_test, 800);
 
 	run_test(fp);
 
+	normalize_waveform();
+	print_waveform(fp, waveform, WAVEFORM_SIZE);
+
+	// 3
+	for(int i = 0; i < 800; i++) {
+		input_test[i] = 120 + 120 * sin(2*M_PI * i / 800);
+	}
+
+	print_waveform(fp, input_test, 800);
+
+	run_test(fp);
+
+	normalize_waveform();
+	print_waveform(fp, waveform, WAVEFORM_SIZE);
 	fclose(fp);
 }
 
--- tests/read_data.py
+++ tests/read_data.py
@@ -1,7 +1,11 @@
 import os
 import matplotlib.pyplot as plt
 import csv
+import sys
 
+show = False
+if len(sys.argv) > 1:
+    show = sys.argv[1] == 's'
 data = []
 
 with open('tests.txt') as csvfile:
@@ -9,34 +13,51 @@ with open('tests.txt') as csvfile:
     for row in reader:
         data.append([int(i) for i in row])
 
+x1 = data[0]
+x2 = range(0,800)
+og_line = data[1]
+exp_line = data[2]
+norm_line = data[3]
+og_sin = data[4]
+exp_sin = data[5]
+norm_sin = data[6]
+og_dc = data[7]
+exp_dc = data[8]
+norm_dc = data[9]
+
 plt.figure()
 plt.title("Simple Comparison of Expansion Algorithm")
 plt.xlabel("Sample Number")
-plt.ylabel("Value [0,799]")
-plt.plot(range(0,800), data[1], 'b')
-plt.plot(data[0], data[2], color='orange')
-plt.plot(range(0,800), data[3], 'g')
-plt.plot(data[0], data[4], color='red')
-plt.legend(['Original linear', 'Expanded linear', 'Original Sine', 'Expanded Sine'])
+plt.ylabel("Value [0,479]")
+plt.plot(x2, og_line, 'b')
+plt.plot(x1, exp_line, color='orange')
+plt.plot(x2, og_sin, 'g')
+plt.plot(x1, exp_sin, color='red')
+plt.plot(x2, og_dc)
+plt.plot(x1, norm_dc)
+plt.legend(['Original linear', 'Expanded linear', 'Original Sine', 'Expanded Sine', 'Original Sine offset', 'Normalized Sine offset'])
 plt.savefig(os.path.join("docs", "combined_expansion_plot.png"))
-#plt.show()
+if show:
+    plt.show()
 
 plt.figure()
 plt.title("Original Signals")
 plt.xlabel("Sample Number")
-plt.ylabel("Value [0,799]")
-plt.plot(range(0,800), data[1], 'b')
-plt.plot(range(0,800), data[3], 'g')
+plt.ylabel("Value [0,479]")
+plt.plot(x2, og_line, 'b')
+plt.plot(x2, og_sin, 'g')
 plt.legend(['Original linear', 'Original Sine'])
 plt.savefig(os.path.join("docs", "original_plot.png"))
-#plt.show()
+if show:
+    plt.show()
 
 plt.figure()
 plt.title("Expanded Signals")
 plt.xlabel("Sample Number")
-plt.ylabel("Value [0,799]")
-plt.plot(data[0], data[2], color='orange')
-plt.plot(data[0], data[4], color='red')
+plt.ylabel("Value [0,479]")
+plt.plot(x1, exp_line, color='orange')
+plt.plot(x1, exp_sin, color='red')
 plt.legend(['Expanded linear', 'Expanded Sine'])
 plt.savefig(os.path.join("docs", "expansion_plot.png"))
-#plt.show()
+if show:
+    plt.show()
